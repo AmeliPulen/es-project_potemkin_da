ed614b85d1783f12595dbd586a87c2db3e26b3df
AmeliPulen <139628158+AmeliPulen@users.noreply.github.com>
2023-11-03 21:44:09 +0300

    Corrected test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,44 +3,48 @@ import pytest
 from src.item import Item
 
 
-item1 = Item("Dildo", 5000, 3)
-item2 = Item("Doll", 20000, 7)
+@pytest.fixture()
+def item():
+    item1 = Item("Dildo", 5000, 3)
+    return item1
 
-# Проверить записались ли данные
-assert Item.all != None
+def test_calculate_total_price(item):
+    # Проверить рассчитывается ли количество
+    assert item.calculate_total_price() == 15000
 
-# Проверить рассчитывается ли количество
-assert item1.calculate_total_price() == 15000
-assert item2.calculate_total_price() == 140000
+def test_apply_discount(item):
+    # Проверить рассчитывается ли скидка
+    Item.pay_rate = 0.5
+    item.apply_discount()
 
-# Проверить рассчитывается ли скидка
-Item.pay_rate = 0.5
-item1.apply_discount()
-item2.apply_discount()
+    assert item.price == 2500
 
-assert item1.price == 2500
-assert item2.price == 10000
+def test_private():
+    # Проверить заприватился ли атрибут
+    item.name = 'VibroDildo'
+    assert item.name == 'VibroDildo'
 
 
-# Проверить заприватился ли атрибут
-item1.name = 'VibroDildo'
-assert item1.name == 'VibroDildo'
+def test_name_length():
+    # Проверить уменьшилась ли длина названия
+    item.name = 'Sex Doll Ann'
+    assert item.name == 'Sex Doll A'
 
-# Проверить уменьшилась ли длина названия
-item2.name = 'Sex Doll Ann'
-assert item2.name == 'Sex Doll A'
+def test_item_quantity():
+    # Проверить количество экземпляров класса созданных из csv файла
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
 
-# Проверить количество экземпляров класса созданных из csv файла
-Item.instantiate_from_csv()
-assert len(Item.all) == 5
+def test_strun_to_number():
+    # Проверить различные типы данных в методе string_to_number
+    assert Item.string_to_number('6') == 6
+    assert Item.string_to_number('6.0') == 6
+    assert Item.string_to_number('6.5') == 6
 
-# Проверить различные типы данных в методе string_to_number
-assert Item.string_to_number('6') == 6
-assert Item.string_to_number('6.0') == 6
-assert Item.string_to_number('6.5') == 6
+def test_repr():
+    # Проверить правильность вызова магического метода __repr__
+    assert repr(item) == "Item('VibroDildo', 2500.0, 3)"
 
-# Проверить правильность вызова магического метода __repr__
-assert repr(item1) == "Item('VibroDildo', 2500.0, 3)"
-
-# Проверить правильность вызова магического метода __str__
-assert str(item1) == 'VibroDildo'
+def test_str():
+    # Проверить правильность вызова магического метода __str__
+    assert str(item) == 'VibroDildo'
